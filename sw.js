07d509a3d929032dbfcdecc029ad76214f416c78
segnoogo <sawadgsalif@gmail.com>
2026-07-01 17:20:24 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Findalyx SYSCO — Service Worker (PWA hors-ligne) */
-var CACHE = 'findalyx-sysco-v20';
+var CACHE = 'findalyx-sysco-v21';
 var CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
